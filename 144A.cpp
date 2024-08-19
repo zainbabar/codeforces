6a1762fe808d032c3e645cfb009aee729105dc86
zain <zain.babar102@gmail.com>
2024-08-19 01:17:32 -0800

    e

---
+++ 144A.cpp
@@ -0,0 +1,24 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+    // orig approach is to find how far the max is from the front
+    // and how far the min is from the back
+    // and if they overlap sub 1 from ans bc it switches once by itself
+    int n;
+    cin>>n;
+    int arr[n];
+    
+    
+    for (int i = 0; i < n; i++){
+       cin>>arr[i];
+    }
+
+    for (int i = 0; i < n; i++){
+    }
+   
+ 
+}
